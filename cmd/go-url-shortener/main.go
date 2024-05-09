993fb36b6b03f718ac492015ae30588e572d62db
Gapil <gapilsoft@gmail.com>
2024-05-10 00:15:52 +0300

    added openapi doc

--- cmd/go-url-shortener/main.go
+++ cmd/go-url-shortener/main.go
@@ -5,10 +5,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/gorilla/mux"
+
 	"github.com/afurgapil/go-url-shortener/configs"
 	"github.com/afurgapil/go-url-shortener/internal/db"
 	"github.com/afurgapil/go-url-shortener/internal/handlers"
-	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -26,19 +28,27 @@ func main() {
 	}
 	defer database.Close()
 
- 	 if db.PingDB(database) {
-        fmt.Println("Veritabanı ping isteği başarılı. Veritabanı erişimi başarılı bir şekilde yapıldı.")
-    }else {
-        log.Fatal("Veritabanı ping isteği başarısız. Veritabanı erişimi sağlanamadı.")
-    }
+	if db.PingDB(database) {
+		fmt.Println("Veritabanı ping isteği başarılı. Veritabanı erişimi başarılı bir şekilde yapıldı.")
+	} else {
+		log.Fatal("Veritabanı ping isteği başarısız. Veritabanı erişimi sağlanamadı.")
+	}
 	fmt.Println("Server is running on localhost:" + config.Port)
 
-  	r := mux.NewRouter()
-	r.HandleFunc("/short",handlers.URLShorter)
-	r.HandleFunc("/delete/{short_url}/{pass_key}",handlers.URLDeleter)
-	r.HandleFunc("/update",handlers.URLUpdate)
-	r.HandleFunc("/log/{id}",handlers.URLLogger)
+	r := mux.NewRouter()
+	r.HandleFunc("/short", handlers.URLShorter)
+	r.HandleFunc("/delete/{short_url}/{pass_key}", handlers.URLDeleter)
+	r.HandleFunc("/update", handlers.URLUpdate)
+	r.HandleFunc("/log/{id}", handlers.URLLogger)
 
-  	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
+	r.PathPrefix("/swagger.yaml").Handler(http.FileServer(http.Dir("./")))
+	opts := middleware.SwaggerUIOpts{SpecURL: "swagger.yaml"}
+	sh := middleware.SwaggerUI(opts, nil)
+	r.Handle("/docs", sh)
+
+	opts1 := middleware.RedocOpts{SpecURL: "swagger.yaml", Path: "doc"}
+	sh1 := middleware.Redoc(opts1, nil)
+	r.Handle("/doc", sh1)
+
+	log.Fatal(http.ListenAndServe(":"+config.Port, r))
 }
